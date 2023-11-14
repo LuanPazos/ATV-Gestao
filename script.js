311724f46d462ac5ebd2b75ee3fe930fa3544d5b
Luan Carlos Pazos Machado <luansp@outlook.com>
2023-11-14 20:53:37 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function validateLogin() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var errorMessage = document.getElementById("error-message");
+
+  // Simples validação de usuário e senha (pode ser feito no servidor em uma aplicação real)
+  if (username === "usuario" && password === "senha") {
+      errorMessage.innerHTML = "Login bem-sucedido!";
+      errorMessage.style.color = "green";
+
+      // Redirecionar para o site desejado
+      window.location.href = "http://aulaqa.000webhostapp.com/";
+  } else {
+      errorMessage.innerHTML = "Usuário ou senha incorretos. Tente novamente.";
+      errorMessage.style.color = "red";
+  }
+}
